0e70a03c2af187813ce1de71eb73a3a4cfe31c03
Fuminori Tsuchiya <tsuchiya.f@tohoku.ac.jp>
2024-12-13 19:45:52 +0900

    update bg list

--- ttools/cp_amateras_bg.sh
+++ ttools/cp_amateras_bg.sh
@@ -21,7 +21,4 @@ cp qsun_bg_low.out /cygdrive/c/opefiles/Debug/qsun_bg_low.out.$1
 cp qsun_bg_low.out qsun_bg_low.out.$1
 cp ../Debug/qsun_bg_tab/archive/*.* /cygdrive/c/opefiles/Debug/qsun_bg_tab/archive/
 scp qsun_bg_*.out.$1 iprt@130.34.116.246:/db/IPRT-SUN/CALIB/
-
-echo "Please update following documents"
-echo " (1) radio.gp.tohoku.ac.jp /home/iprt/amateras/ql/bg_list.dat"
-echo " (2) radio.gp.tohoku.ac.jp /home/iprt/amateras/ql/bg_list.history"
+scp bg_list.history iprt@130.34.116.246:/db/IPRT-SUN/CALIB/
